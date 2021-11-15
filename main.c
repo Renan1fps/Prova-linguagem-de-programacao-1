98e93629b0c8f5778bc5a30766cee4e18c786306
Renan Oliveira <renan.sergipe68@gmail.com>
2021-11-15 19:06:18 -0300

    Fix: corrreçao de bugs

--- main.c
+++ main.c
@@ -3,43 +3,47 @@
 
 struct aluno {
 
-    char nome[100];
-    int idade;
-    double nota1;
-    double nota2;
+    char nome[200];
+    float nota1;
+    float nota2;
 }
 strAluno[3];
 
 int main()
 {
+    const int TAMANHO = 3;
     bool condicao = true;
     int valor, quantidadeParaCadastro, arrayEstaCheio = 0;
     
 
     while(condicao){
-        printf("\nDigite o número de uma das opções abaixo ");
+        printf("\n\nDigite o número de uma das opções abaixo ");
         printf("\n1-Cadastrar um novo estudante ");
         printf("\n2-Listar todos os dados dos estudantes cadastrados ");
-        printf("\n3-Sair\n");
+        printf("\n3-Buscar aluno por nome");
+        printf("\n4-Sair\n");
         scanf("%d", &valor);
         
         switch(valor){
             
             case 1: {
-
+                
                 printf("Quantos alunos deseja cadastrar?");
                 scanf("%d", &quantidadeParaCadastro);
  
-                if(arrayEstaCheio > 3 || quantidadeParaCadastro > 3){
+                if(arrayEstaCheio > TAMANHO || quantidadeParaCadastro > TAMANHO){
                     printf("\nNão é possível cadastrar alunos\n");
                     break;
                 }
 
                 for (int i = 0; i < quantidadeParaCadastro; i++) {
-                    printf("\nDigite o nome do aluno: ");
-                    scanf("%s", strAluno[i].nome);
-                    printf("\nDigite a idade do aluno: ");
-                    scanf("%d", &strAluno[i].idade);
+                    setbuf(stdin, NULL);
+                    printf("\nDigite o nome completo do aluno %d:", i+1);
+                    scanf("%[^\n]", strAluno[i].nome); 
+                    printf("\nDigite a nota 1 do aluno: ");
+                    scanf("%f", &strAluno[i].nota1);
+                    printf("\nDigite a nota 2 do aluno: ");
+                    scanf("%f", &strAluno[i].nota2);
 
                     arrayEstaCheio += quantidadeParaCadastro;
                 }
@@ -47,18 +51,41 @@ int main()
             }
             
             case 2: {
+
                 printf("\n>>>>Alunos cadastrados<<<<");
-                for (int i = 0; i < 3; i++) {
+                for (int i = 0; i < TAMANHO; i++) {
+                    if(strAluno[i].nome != 0){
                     printf("\nAluno %d", i+1);
                     printf("\nNome do aluno: %s", strAluno[i].nome);
-                    printf("\nIdade do aluno: %d", strAluno[i].idade);
+                    printf("\nNota 1 do aluno: %f", strAluno[i].nota1);
+                    printf("\nNota 2 do aluno: %f", strAluno[i].nota2);
                     printf("\n--------------------------------------");
+                    }
                 }
                 break;
             }
             
             case 3:{
+
+                char buscarAlunoPorNome[200];
+                printf("Digite o nome do aluno: ");
+                scanf("%s", buscarAlunoPorNome);
+
+                for (int i = 0; i < TAMANHO; i++){
+                    if(strAluno[i].nome == buscarAlunoPorNome){
+                        printf("\nNome do aluno: %s", strAluno[i].nome);
+                        printf("\nNota 1 do aluno: %f", strAluno[i].nota1);
+                        printf("\nNota 2 do aluno: %f", strAluno[i].nota2);
+                    }else{
+                        printf("\nAluno não encontrado");
+                    }
+                }
+                break;
+            }
+
+            case 4:{
                 condicao = false;
+                break;
             }
             
         }
@@ -66,3 +93,4 @@ int main()
 
     return 0;
 }
+
